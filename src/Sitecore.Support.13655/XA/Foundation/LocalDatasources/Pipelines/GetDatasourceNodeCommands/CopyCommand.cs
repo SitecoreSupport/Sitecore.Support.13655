d718d4e3cb3239c2426cac85f9252013cd682946
DK\IRO <iro@sitecore.net>
2018-09-06 13:12:41 +0300

    Initial

---
+++ src/Sitecore.Support.13655/XA/Foundation/LocalDatasources/Pipelines/GetDatasourceNodeCommands/CopyCommand.cs
@@ -0,0 +1,53 @@
+﻿namespace Sitecore.Support.XA.Foundation.LocalDatasources.Pipelines.GetDatasourceNodeCommands
+{
+  using Microsoft.Extensions.DependencyInjection;
+  using Sitecore.Abstractions;
+  using Sitecore.Data;
+  using Sitecore.Data.Items;
+  using Sitecore.DependencyInjection;
+  using Sitecore.Globalization;
+  using Sitecore.XA.Foundation.LocalDatasources.Pipelines.GetDatasourceNodeCommands;
+  using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+  using Sitecore.XA.Foundation.SitecoreExtensions.Repositories;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  public class CopyCommand
+  {
+    private IEnumerable<ID> _excludeItems;
+
+    protected IEnumerable<ID> ExcludeItems
+    {
+      get
+      {
+        if (_excludeItems == null)
+        {
+          LazyResetable<BaseFactory> requiredResetableService = ServiceLocator.GetRequiredResetableService<BaseFactory>();
+          _excludeItems = from s in requiredResetableService.Value.GetStringSet("experienceAccelerator/datasourceCopyCommand/excludedTemplates/*")
+                          select new ID(s);
+        }
+        return _excludeItems;
+      }
+    }
+
+    public void Process(GetDataSourceNodeCommandsArgs args)
+    {
+      if (ShouldExecute(ID.Parse(args.ItemId)))
+      {
+        Database database = ServiceLocator.ServiceProvider.GetService<IDatabaseRepository>().GetDatabase(args.DatabaseName);
+        ID iD = ID.Parse(args.ItemId);
+        Item item = database.GetItem(iD);
+        if (item != null && !item.InheritsFrom(Sitecore.XA.Foundation.Presentation.Templates.PartialDesignFolder.ID) && !item.InheritsFrom(Sitecore.XA.Foundation.Presentation.Templates.PartialDesign.ID) && !item.Parent.InheritsFrom(Sitecore.XA.Foundation.Multisite.Templates.Data.ID))
+        {
+          string item2 = string.Format("<a href=\"#\" onclick=\"javascript:return scForm.postEvent(this,event,'CopyDataSource(&amp;quot;{0}&amp;quot;)')\">{1}</a>", iD, Translate.Text("Copy To"));
+          args.Commands.Add(item2);
+        }
+      }
+    }
+
+    protected virtual bool ShouldExecute(ID itemId)
+    {
+      return !ExcludeItems.Contains(itemId);
+    }
+  }
+}
---
+++ src/Sitecore.Support.13655/XA/Foundation/LocalDatasources/Pipelines/GetDatasourceNodeCommands/CreateCommand.cs
@@ -0,0 +1,40 @@
+﻿namespace Sitecore.Support.XA.Foundation.LocalDatasources.Pipelines.GetDatasourceNodeCommands
+{
+  using Microsoft.Extensions.DependencyInjection;
+  using Sitecore.Data;
+  using Sitecore.Data.Items;
+  using Sitecore.Data.Masters;
+  using Sitecore.DependencyInjection;
+  using Sitecore.Globalization;
+  using Sitecore.XA.Foundation.LocalDatasources;
+  using Sitecore.XA.Foundation.LocalDatasources.Pipelines.GetDatasourceNodeCommands;
+  using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+  using Sitecore.XA.Foundation.SitecoreExtensions.Repositories;
+  using System.Linq;
+
+  public class CreateCommand
+  {
+    public void Process(GetDataSourceNodeCommandsArgs args)
+    {
+      Database database = ServiceLocator.ServiceProvider.GetService<IDatabaseRepository>().GetDatabase(args.DatabaseName);
+      if (database != null)
+      {
+        string text = ID.Parse(args.ItemId).ToString();
+        Item item = database.GetItem(text);
+        if (item != null && !text.Equals(Items.VirtualPageStandardValue.ToString()) && !item.InheritsFrom(Sitecore.XA.Foundation.Presentation.Templates.PartialDesignFolder.ID))
+        {
+          string item2 = string.Empty;
+          if (item.InheritsFrom(Items.VirtualPageData) || item.InheritsFrom(Sitecore.XA.Foundation.Editing.Templates.PageDataFolder))
+          {
+            item2 = string.Format("<a href=\"#\" onclick=\"javascript:return scForm.postEvent(this,event,'CreateLocalDataSource(&amp;quot;{0}&amp;quot;)')\">{1}</a>", text, Translate.Text("Create"));
+          }
+          else if (Masters.GetMasters(item).Any())
+          {
+            item2 = string.Format("<a href=\"#\" onclick=\"javascript:return scForm.postEvent(this,event,'CreateDataSource(&amp;quot;{0}&amp;quot;)')\">{1}</a>", text, Translate.Text("Create"));
+          }
+          args.Commands.Add(item2);
+        }
+      }
+    }
+  }
+}
